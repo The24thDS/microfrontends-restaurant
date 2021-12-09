56150ca4369c702e5087ddb9210a7cf37d41b8e4
David Sima <davids@ensemble.com>
2021-12-09 11:18:59 +0200

    prepared as microfrontend

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import CustomBrowserRouter from "./CustomBrowserRouter";
 
 const App = ({ history = createBrowserHistory() }) => {
   return (
-    <CustomBrowserRouter history={history}>
-      <nav>
-        Restaurant Page Nav: <NavLink to="restaurants">Restaurants</NavLink>{" "}
-        <NavLink to="restaurants/1">Restaurant 1</NavLink>
-      </nav>
+    <CustomBrowserRouter basename="restaurants" history={history}>
+      <p>
+        <nav>
+          Restaurant Page Nav: <NavLink to="/">Root</NavLink>{" "}
+          <NavLink to="1">Restaurant 1</NavLink>{" "}
+          <NavLink to="/../">Back to the main page</NavLink>
+        </nav>
+      </p>
       <div>
         <Routes>
-          <Route path="restaurants/" element={<p>Restaurants Page</p>} />
-          <Route path="restaurants/*" element={<p>Restaurant subpage</p>} />
+          <Route path="/" element={<p>Restaurants Page</p>} />
+          <Route path="/*" element={<p>Restaurant subpage</p>} />
         </Routes>
       </div>
     </CustomBrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+window.render_restaurants = (elementId, history, host) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(elementId)
+  );
+};
+
+window.unmount_restaurants = (elementId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(elementId));
+};
+
+if (document.getElementById("restaurants-container") === null) {
+  ReactDOM.render(<App />, document.getElementById("app"));
+}
